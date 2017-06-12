c5293c5cba243c9bdac4cb238d539825af8d61ac
Jason Borowski <esc0rtd3w@gmail.com>
2017-06-12 03:29:12 -0400

    Split Categories and Added PSN NPXX/BXXX Combo Downloding

--- ps3-pkg-checker.cmd
+++ ps3-pkg-checker.cmd
@@ -30,8 +30,15 @@ set titleIDRegionDisc=BXXX
 set titleIDRegionPSN=NPXX
 set titleIDNumber=00000
 set titleIDNumberStart=0
+set titleIDNumberMin=00000
+set titleIDNumberMax=99999
 set isRegion=0
 set padding=0
+
+set isBlankXML=0
+
+set titleIDPSN=0
+set isBlankXMLPSN=0
 
 set prefixURL=https://
 set serverA=%prefixURL%a0.ww.np.dl.playstation.net/tpl/np/%titleID%/%titleID%-ver.xml
@@ -53,20 +60,23 @@ echo.
 echo 1) Check All Possible Title IDs
 echo.
 echo 2) Check All PSN Title IDs
-echo.
 echo 3) Check All Disc Title IDs
 echo.
-echo 4) Check All JAPAN Title IDs
-echo 5) Check All USA Title IDs
-echo 6) Check All EUROPE Title IDs
-echo 7) Check All ASIA Title IDs
-echo 8) Check All HK Title IDs
+echo 4) Check All JAPAN Title IDs [BLJM/NPJB]
+echo 4a) Check All JAPAN Title IDs [BLJS/NPJA]
+echo 4b) Check All JAPAN Title IDs [BCJS]
+echo 5) Check All USA Title IDs [BLUS/NPUB]
+echo 5a) Check All USA Title IDs [BCUS/NPUA]
+echo 6) Check All EUROPE Title IDs [BLES/NPEB]
+echo 6a) Check All EUROPE Title IDs [BCES/NPEA]
+echo 7) Check All ASIA Title IDs [BLAS/NPHB]
+echo 7a) Check All ASIA Title IDs [BCAS/NPHA]
+echo 8) Check All HK Title IDs [BLKS/NPKB]
+echo 8a) Check All HK Title IDs [BCKS/NPKA]
 echo.
 echo 9) Enter Custom Title ID
 echo.
-echo.
 echo X) Exit Menu
-echo.
 echo.
 
 set /p titleChoice=
@@ -81,10 +91,22 @@ if %titleChoice%==1 goto all
 if %titleChoice%==2 goto psn
 if %titleChoice%==3 goto disc
 if %titleChoice%==4 set isRegion=JPN&&goto region
+if %titleChoice%==4a set isRegion=JPN2&&goto region
+if %titleChoice%==4A set isRegion=JPN2&&goto region
+if %titleChoice%==4b set isRegion=JPN3&&goto region
+if %titleChoice%==4B set isRegion=JPN3&&goto region
 if %titleChoice%==5 set isRegion=USA&&goto region
+if %titleChoice%==5a set isRegion=USA2&&goto region
+if %titleChoice%==5A set isRegion=USA2&&goto region
 if %titleChoice%==6 set isRegion=EUR&&goto region
+if %titleChoice%==6a set isRegion=EUR2&&goto region
+if %titleChoice%==6A set isRegion=EUR2&&goto region
 if %titleChoice%==7 set isRegion=ASIA&&goto region
+if %titleChoice%==7a set isRegion=ASIA2&&goto region
+if %titleChoice%==7A set isRegion=ASIA2&&goto region
 if %titleChoice%==8 set isRegion=HK&&goto region
+if %titleChoice%==8a set isRegion=HK2&&goto region
+if %titleChoice%==8A set isRegion=HK2&&goto region
 if %titleChoice%==9 goto custom
 
 
@@ -143,45 +165,91 @@ echo.
 echo.
 
 :: Region Start
-if %isRegion%==JPN set dumpPath=%root%\dump\JPN
-if %isRegion%==JPN set titleIDNumber=55000
-::if %isRegion%==JPN set titleIDRegionDisc=BCJS
-if %isRegion%==JPN set titleIDRegionDisc=BLJM
-if %isRegion%==JPN set titleIDRegionPSN=NPJB
-::if %isRegion%==JPN set titleIDRegionPSN=NPJA
+if %isRegion%==JPN (
+set dumpPath=%root%\dump\JPN
+set titleIDNumber=55000
+set titleIDNumber2=60000
+set titleIDRegionDisc=BLJM
+set titleIDRegionPSN=NPJB
+)
 
-if %isRegion%==EUR set dumpPath=%root%\dump\EUR
-if %isRegion%==EUR set titleIDNumber=00000
-::if %isRegion%==EUR set titleIDRegionDisc=BCES
-if %isRegion%==EUR set titleIDRegionDisc=BLES
-if %isRegion%==EUR set titleIDRegionPSN=NPEB
-::if %isRegion%==EUR set titleIDRegionPSN=NPEA
+if %isRegion%==JPN2 (
+set dumpPath=%root%\dump\JPN
+set titleIDNumber=55000
+set titleIDNumber2=60000
+set titleIDRegionDisc=BLJS
+set titleIDRegionPSN=NPJA
+)
 
-if %isRegion%==USA set dumpPath=%root%\dump\USA
-if %isRegion%==USA set titleIDNumber=30000
-::if %isRegion%==USA set titleIDRegionDisc=BCUS
-if %isRegion%==USA set titleIDRegionDisc=BLUS
-if %isRegion%==USA set titleIDRegionPSN=NPUB
-::if %isRegion%==USA set titleIDRegionPSN=NPUA
+if %isRegion%==JPN3 (
+set dumpPath=%root%\dump\JPN
+set titleIDNumber=55000
+set titleIDNumber2=60000
+set titleIDRegionDisc=BCJS
+)
 
-if %isRegion%==HK set dumpPath=%root%\dump\HK
-if %isRegion%==HK set titleIDNumber=20000
-::if %isRegion%==HK set titleIDRegionDisc=BCKS
-if %isRegion%==HK set titleIDRegionDisc=BLKS
-if %isRegion%==HK set titleIDRegionPSN=NPKB
-::if %isRegion%==HK set titleIDRegionPSN=NPKA
+if %isRegion%==EUR (
+set dumpPath=%root%\dump\EUR
+set titleIDNumber=00000
+set titleIDRegionDisc=BLES
+set titleIDRegionPSN=NPEB
+)
 
-if %isRegion%==ASIA set dumpPath=%root%\dump\ASIA
-if %isRegion%==ASIA set titleIDNumber=50000
-::if %isRegion%==ASIA set titleIDRegionDisc=BCAS
-if %isRegion%==ASIA set titleIDRegionDisc=BLAS
-if %isRegion%==ASIA set titleIDRegionPSN=NPHB
-::if %isRegion%==ASIA set titleIDRegionPSN=NPHA
+if %isRegion%==EUR2 (
+set dumpPath=%root%\dump\EUR
+set titleIDNumber=00000
+set titleIDRegionDisc=BCES
+set titleIDRegionPSN=NPEA
+)
+
+if %isRegion%==USA (
+set dumpPath=%root%\dump\USA
+set titleIDNumber=30000
+set titleIDRegionDisc=BLUS
+set titleIDRegionPSN=NPUB
+)
+
+if %isRegion%==USA2 (
+set dumpPath=%root%\dump\USA
+set titleIDNumber=30000
+set titleIDRegionDisc=BCUS
+set titleIDRegionPSN=NPUA
+)
+
+if %isRegion%==HK (
+set dumpPath=%root%\dump\HK
+set titleIDNumber=20000
+set titleIDRegionDisc=BLKS
+set titleIDRegionPSN=NPKB
+)
+
+if %isRegion%==HK2 (
+set dumpPath=%root%\dump\HK
+set titleIDNumber=20000
+set titleIDRegionDisc=BCKS
+set titleIDRegionPSN=NPKA
+)
+
+if %isRegion%==ASIA (
+set dumpPath=%root%\dump\ASIA
+set titleIDNumber=50000
+set titleIDRegionDisc=BLAS
+set titleIDRegionPSN=NPHB
+)
+
+if %isRegion%==ASIA2 (
+set dumpPath=%root%\dump\ASIA
+set titleIDNumber=50000
+set titleIDRegionDisc=BCAS
+set titleIDRegionPSN=NPHA
+)
 
 set isLoop=1
 set return=regionL
 
 :regionL
+if %titleIDNumber%==%titleIDNumberMax% goto region
+
 if %isLoop%==1 set /a titleIDNumber=%titleIDNumber%+1
 
 setlocal ENABLEDELAYEDEXPANSION
@@ -205,9 +273,11 @@ endlocal
 set /p padding=<"%temp%\padding.tmp"
 
 set titleID=%titleIDRegionDisc%%padding%%titleIDNumber%
+set titleIDPSN=%titleIDRegionPSN%%padding%%titleIDNumber%
 if %padding%==none set titleID=%titleIDRegionDisc%%titleIDNumber%
+if %padding%==none set titleIDPSN=%titleIDRegionPSN%%titleIDNumber%
 
-echo %titleID%
+echo %titleID% / %titleIDPSN%
 echo.
 ::pause
 
@@ -232,13 +302,16 @@ goto setServer
 
 :setServer
 set serverA=%prefixURL%a0.ww.np.dl.playstation.net/tpl/np/%titleID%/%titleID%-ver.xml
+set serverAPSN=%prefixURL%a0.ww.np.dl.playstation.net/tpl/np/%titleIDPSN%/%titleIDPSN%-ver.xml
 set serverB=%prefixURL%b0.ww.np.dl.playstation.net/tppkg/np/%titleID%/%titleID%-ver.xml
+set serverBPSN=%prefixURL%b0.ww.np.dl.playstation.net/tppkg/np/%titleIDPSN%/%titleIDPSN%-ver.xml
 
 goto dlPkg
 
 
 :dlPkg
 %wget% %disableCertCheck% %userAgent% -O "%dumpPath%\%titleID%.xml" %serverA%
+%wget% %disableCertCheck% %userAgent% -O "%dumpPath%\%titleIDPSN%.xml" %serverAPSN%
 
 goto chkBlank
 
@@ -249,6 +322,14 @@ for %%a in ("%dumpPath%\%titleID%.xml") do (
 	set isBlankXML=1
   ) else (
 	set isBlankXML=0
+  )
+)
+
+for %%a in ("%dumpPath%\%titleIDPSN%.xml") do (
+  if %%~za equ 0 (
+	set isBlankXMLPSN=1
+  ) else (
+	set isBlankXMLPSN=0
   )
 )
 
@@ -265,6 +346,17 @@ if %isBlankXML%==1 (
 echo %titleID%>>%updateListComplete%
 echo %titleID%>>%updateListFail%
 del /f /q "%dumpPath%\%titleID%.xml"
+)
+
+if %isBlankXMLPSN%==0 (
+echo %titleIDPSN%>>%updateListComplete%
+echo %titleIDPSN%>>%updateListActive%
+)
+
+if %isBlankXMLPSN%==1 (
+echo %titleIDPSN%>>%updateListComplete%
+echo %titleIDPSN%>>%updateListFail%
+del /f /q "%dumpPath%\%titleIDPSN%.xml"
 )
 
 
